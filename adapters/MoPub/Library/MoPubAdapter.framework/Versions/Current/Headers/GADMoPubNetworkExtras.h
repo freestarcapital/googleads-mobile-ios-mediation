4946503ba2fbd0cd0256928ae0fe631a96410372
Dean Chang <dean.chang@freestar.io>
2020-01-15 20:23:12 -0800

    Update to MoPub project file.

--- adapters/MoPub/Library/MoPubAdapter.framework/Versions/Current/Headers/GADMoPubNetworkExtras.h
+++
@@ -1,9 +0,0 @@
-@import Foundation;
-@import GoogleMobileAds;
-
-@interface GADMoPubNetworkExtras : NSObject<GADAdNetworkExtras>
-
-/// Holds the privacy icon size in points for the MoPub's native ad.
-@property(assign) float privacyIconSize;
-
-@end
